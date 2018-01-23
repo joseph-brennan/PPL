985da2ae517c152aef1c4d7d8275c695ed02beea
Joey <sharpshooter30@gmail.com>
2018-01-23 10:21:38 -0700

    starting the lowest level of snub

---
+++ Mal_checking/MAL Checking.py
@@ -0,0 +1,38 @@
+"""
+Joey Brennan
+Project 1 Mal Syntax Checking
+"""
+
+
+class MalChecking:
+    def __init__(self):
+        pass
+
+    def error_checking(self):
+        pass
+
+    def bad_label(self):
+        pass
+
+    def invalid_opcode(self):
+        pass
+
+    def bad_high_operands(self):
+        pass
+
+    def bad_low_operands(self):
+        pass
+
+    def wrong_operands(self):
+        pass
+
+    def bad_type(self):
+        pass
+
+    def warning_label(self):
+        pass
+
+
+if __name__ == '__main__':
+    Mal = MalChecking
+    print("hello world")
