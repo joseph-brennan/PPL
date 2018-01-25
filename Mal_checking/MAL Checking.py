4a9faa0d2d49d39a29444a762d72051c96590c93
Joey <sharpshooter30@gmail.com>
2018-01-25 14:32:07 -0700

    working in the logic of how i think th loops should work

--- Mal_checking/MAL Checking.py
+++ Mal_checking/MAL Checking.py
@@ -7,35 +7,103 @@ Project 1 Mal Syntax Checking
 class MalChecking:
     def __init__(self, file_name):
         self.file_name = file_name
-        self.mal_file = open(file_name, 'r+')
 
-    def error_checking(self):
+        self.mal_file = open(file_name, 'r')
+
+        self.log = open("log_file.txt", 'w')
+
+    def read_file(self):
+        for l in self.mal_file:
+
+            # blank line move forward
+            if l.__len__() is 0:
+                continue
+
+            # skip the comments
+            elif l[0] is ';':
+                continue
+
+            else:
+                self.error_checking(l)
+
+        self.mal_file.close()
+
+        self.file_end()
+
+    def error_checking(self, line):
+        self.bad_label(line)
+
+        self.invalid_opcode(line)
+
+        self.bad_low_operands(line)
+
+        self.bad_high_operands(line)
+
+        self.bad_type(line)
+
+        self.warning_label(line)
+
+    def bad_label(self, line):
+        for i in line:
+            if i is ':':
+                pass
+                # do check this is a label
+            else:
+                continue
+
+    def invalid_opcode(self, line):
+        while True:
+            word = ""
+            for letter in line:
+                word += letter
+
+                if word is "MOVEI":
+                    pass
+                elif word is "MOVE":
+                    pass
+                elif word is "ADD":
+                    pass
+                elif word is "INC":
+                    pass
+                elif word is "SUB":
+                    pass
+                elif word is "DEC":
+                    pass
+                elif word is "MUL":
+                    pass
+                elif word is "DIV":
+                    pass
+                elif word is "BEQ":
+                    pass
+                elif word is "BLT":
+                    pass
+                elif word is "BGT":
+                    pass
+                elif word is "BR":
+                    pass
+                else:
+                    pass
+
+    def bad_high_operands(self, line):
         pass
 
-    def bad_label(self):
+    def bad_low_operands(self, line):
         pass
 
-    def invalid_opcode(self):
+    def wrong_operands(self, line):
         pass
 
-    def bad_high_operands(self):
+    def bad_type(self, line):
         pass
 
-    def bad_low_operands(self):
+    def warning_label(self, line):
         pass
 
-    def wrong_operands(self):
-        pass
-
-    def bad_type(self):
-        pass
-
-    def warning_label(self):
-        pass
+    def file_end(self):
+        self.log.close()
 
 
 if __name__ == '__main__':
-    file_name = input("what is the name of the mal file?")
-
+    file_name = "mal_test"  # input("what is the name of the mal file?")
+    file_name += '.mal'
     mal = MalChecking(file_name)
-
