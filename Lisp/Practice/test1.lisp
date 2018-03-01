91bbbbed5fe6f284f43d5927625e45c6ea5a2ed8
Joey <sharpshooter30@gmail.com>
2018-03-01 08:53:40 -0700

    wrote a git ignore and adding lisp folder to the repository

---
+++ Lisp/Practice/test1.lisp
@@ -0,0 +1,15 @@
+;;;; Welcome to Portacle, the Portable Common Lisp Environment.
+;;; Hi
+;; For information on Portacle and how to use it, please read the website at
+;;   https://github.com/Shinmera/portacle
+;; or see the *portacle-help* buffer. You can switch to it by pressing this:
+;;   Ctrl+h h
+;; or by clicking on the *scratch* field below until it changes to read
+;; *portacle-help*.
+;;
+;; You can use this buffer for notes and tinkering with small pieces of code.
+
+;; testing
+(defun testing ()
+  (car '(a b c)))
+(testing)
