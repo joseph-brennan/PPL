c33ca834dc61ea12ecb5882afc9c554e948b99f7
Joey <sharpshooter30@gmail.com>
2018-03-12 17:34:05 -0600

    it works making changes

--- Lisp/project 3/count-clumps.lisp
+++ Lisp/project 3/count-clumps.lisp
@@ -8,16 +8,40 @@
 ;;       1. the list contains no nested lists
 
 (defun count-clumps (lst)
-  (cond ((null lst)                                                 0)
-        ((and (atom (car lst)) (equal (car lst) (car (cdr lst))))  (+ 1 (count-clumps (cdr lst))))
-        ((and (numberp (car lst)) (= (car lst) (car (cdr lst))))   (+ 1 (count-clumps (cdr lst))))
-        (t                                                         (count-clumps (cdr lst)))
+  (cond ((null lst)             0)
+        ((and (atom (car lst)) (clump (car lst) (cdr lst)))      (+ 1 (matcha (car lst) (cdr lst))))
+        ((and (numberp (car lst)) (clump (car lst) (cdr lst)))   (+ 1 (matchn (car lst) (cdr lst))))
+        (t                                                        (count-clumps (cdr lst)))
   )
 )
 
-(defun matcha (at lst))
+(defun matchan (val lst)
+  (cond ((null lst)                               (count-clumps lst))
+        ((and (atom val) (equal val (car lst)))   (matchan at (cdr lst)))
+        ((and (numberp val) (= val (car lst)))    (matchan val (cdr lst)))
+        (t                                        (count-clumps lst))))
 
-(defun matchn (num lst))
+(defun matcha (at lst)
+  (cond ((null lst)                (count-clumps lst))
+        ((equal at (car lst))      (matcha at (cdr lst)))
+        (t                         (count-clumps lst))
+  )
+)
+
+(defun matchn (num lst)
+  (cond ((null lst)          (count-clumps lst))
+        ((= num (car lst))   (matchn num (cdr lst)))
+        (t                   (count-clumps lst))
+  )
+)
+
+(defun clump (val lst)
+  (cond ((null lst)     nil)
+        ((and (atom val) (equal val (car lst)))     t)
+        ((and (numberp val) (= val (car lst)))      t)
+        (t                                          nil)
+  )
+)
 
 ;;  test plan for count groups:
 ;;  category / description		data		expected result
